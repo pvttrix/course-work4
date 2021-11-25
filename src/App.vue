9eaa5447155a293f8eac31d96f1c776b0adb6679
pvttrix <dipatricha@gmail.com>
2021-11-25 23:25:16 +0300

    add basic matching

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
 <section class="game-board">
   <Card
       v-for="(card, index) in cardList"
+      :matched="card.matched"
       :value="card.value"
       :key="`card-${index}`"
       :visible="card.visible"
       :position="card.position"
       @select-card="flipCard"
   />
+
 </section>
+  <h2> {{status}}</h2>
 </template>
 
 <script>
 import  Card from './components/Card'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 export default {
   name: 'App',
   components: {
@@ -21,21 +24,51 @@ export default {
   },
   setup() {
     const cardList = ref([])
+    const userSelection = ref([])
+    const status = ref('')
 
     for(let i = 0; i < 16; i++) {
       cardList.value.push({
         value: i,
         visible:false,
-        position: i
+        position: i,
+        matched: false
       })
     }
 
     const flipCard = (payload) => {
       cardList.value[payload.position].visible = true
+
+      if (userSelection.value[0]) {
+        userSelection.value[1] = payload
+      } else {
+        userSelection.value[0] = payload
+      }
     }
+
+    watch(userSelection, (currentValue) => {
+      if (currentValue.length === 2) {
+
+        const cardOne = currentValue[0]
+        const cardTwo = currentValue[1]
+
+        if(cardOne.faceValue === cardTwo.faceValue) {
+          cardList.value[cardOne.position].matched = true
+          cardList.value[cardTwo.position].matched = true
+        } else {
+          status.value = 'dismatch'
+          cardList.value[cardOne.position].visible = false
+          cardList.value[cardTwo.position].visible = false
+        }
+
+        userSelection.value.length = 0
+      }
+    }, { deep: true })
     return {
       cardList,
-      flipCard
+      flipCard,
+      userSelection,
+      status
     }
   }
 }
